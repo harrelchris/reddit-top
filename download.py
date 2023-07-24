cf68baa265d9b56638de9eeeec00845be57470ab
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-07-24 02:43:45 -0500

    add error handling

--- download.py
+++ download.py
@@ -1,6 +1,8 @@
 import datetime
 import json
 import pathlib
+import sys
+import urllib.error
 import urllib.request
 
 attrs = [
@@ -20,21 +22,34 @@ attrs = [
     "subreddit",
     "author",
 ]
-posts = []
 
-with urllib.request.urlopen("https://www.reddit.com/r/all/top.json?sort=top&t=day") as response:
-    response_body = response.read()
-    response_json = json.loads(response_body)
-    response_data = response_json["data"]
-    children = response_data["children"]
-    for child in children:
-        data = child["data"]
-        post = {attr: data[attr] for attr in attrs}
-        posts.append(post)
+posts = []
+attempts = 0
+
+while True:
+    try:
+        response = urllib.request.urlopen("https://www.reddit.com/r/all/top.json?sort=top&t=day")
+    except urllib.error.HTTPError as e:
+        attempts += 1
+        if attempts > 3:
+            sys.stderr.write(f"Failed after 3 attempts: {e}\n")
+            sys.exit(1)
+        continue
+    else:
+        response_body = response.read()
+        response_json = json.loads(response_body)
+        response_data = response_json["data"]
+        children = response_data["children"]
+        for child in children:
+            data = child["data"]
+            post = {attr: data[attr] for attr in attrs}
+            posts.append(post)
+        response.close()
+        break
 
 DATA = pathlib.Path(__file__).parent / "data"
 DATE = datetime.datetime.now().strftime("%Y-%m-%d")
 fp = f"{DATA / DATE}.json"
 
 with open(fp, "w", encoding="utf-8") as f:
-    json.dump(post, f)
+    json.dump(posts, f)
